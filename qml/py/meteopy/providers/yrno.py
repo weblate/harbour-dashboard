d43e68a6a91d2bcf2e11bb29b6b8324ddb46dd8c
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2022-07-28 18:06:12 +0200

    Properly prepare yrno provider

--- qml/py/meteopy/providers/yrno.py
+++ qml/py/meteopy/providers/yrno.py
@@ -4,5 +4,23 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-class Provider:
-    pass
+from .provider_base import Capability
+from .provider_base import Provider as ProviderBase
+
+
+class Provider(ProviderBase):
+    name = 'Yr.no'
+    handle = 'yrn'
+    capabilities = Capability.SUMMARY_FORECAST | Capability.DETAILED_FORECAST |\
+        Capability.PRECIPITATION | Capability.TEMPERATURE
+
+    def __init__(self, signal_callback, log_callback):
+        super().__init__(signal_callback, log_callback)
+        self._setup()
+
+    def refresh(self, ident: str, force: bool) -> None:
+        raise NotImplementedError("Provider backend is not yet implemented")
+
+    def _setup(self):
+        self.ready = False
+        raise NotImplementedError("Provider backend is not yet implemented")
